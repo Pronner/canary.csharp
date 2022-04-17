f5af69fd62b7bc10eb0e9d1100e20c11a5515068
Pronner <pronnerdev@gmail.com>
2022-04-17 23:01:22 +0400

    autoupdate (#8)
    autoupdate (#8)

---
+++ update.cs
@@ -0,0 +1,57 @@
+using System;
+using CanaryAPI;
+using System.IO;
+using System.Net;
+
+namespace Canary
+{
+	
+	public partial class update : Class
+	{
+
+		public update()
+		{
+
+			WebClient updateClient = new WebClient();
+
+			if (!updateClient.DownloadString(CanaryAPI.stringUpdURI).Contains("x10.1"))
+			{
+
+				updateClient.DownloadFile(CanaryAPI.stringDownloadURI, @"C:\Desktop");
+				return;
+
+			}
+
+			// finish
+			try
+			{
+
+				this.Close();
+
+			}
+			catch 
+			{
+
+				try
+				{
+
+					Application.Exit();
+
+				}
+				catch
+				{
+
+					// oops
+					string oopsie = "seems like the class isn't closing";
+
+					Console.WriteLine(oopsie);
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
