737a222e7de0d01efb9436a7d10e0f4d5276df3a
Pronner <pronnerdev@gmail.com>
2022-04-17 21:28:50 +0400

    Definitions for Canary.csharp v1.2 (#1)
    Definitions for Canary.csharp v1.2 (#1)

---
+++ src/bot/definitions.cs
@@ -0,0 +1,45 @@
+/* Canary.CSharp | Definitions */
+
+
+using System;
+using CanaryAPI;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Canary
+{
+	
+	public partial class Definitions : Class
+	{
+
+		public Definitions()
+		{
+
+			// libDefinitions
+			CheckForIllegalCrossThreadCalls = false;
+
+		}
+
+		public void sendMessage(string message, string channelID, string messageType) 
+		{
+
+			CanaryAPI.sendServerDataAsMessage(message, channelID, messageType);
+
+		}
+
+		public void kickMember(string memberUsername, string timeKicked = null)
+		{
+
+			CanaryAPI.kickServerMember(memberUsername, timeKicked);
+
+		}
+
+		public void banMember(string memberUsername, string timeBanned = null)
+		{
+
+			CanaryAPI.banServerMember(memberUsername, timeBanned);
+
+		}
+	}
+
+}
