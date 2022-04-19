fa88a84e71f66247974b78178b1198c0f685dc78
Pronner <pronnerdev@gmail.com>
2022-04-19 15:38:14 +0400

    added error handling (#1)
    added error handling (#1)

---
+++ src/errorhandler.cs
@@ -0,0 +1,40 @@
+using System;
+using CanaryAPI;
+using System.IO;
+using System.Net;
+
+namespace Canary
+{
+
+	public partial class errorhandler : Class
+	{
+
+		public errorhandler()
+		{
+
+			if (CanaryAPI.errorReceived != string.Empty) 
+			{
+
+			    CanaryAPI.handleError();
+
+				CanaryAPI.errorReceived.ToString();
+
+				CanaryAPI.showConsoleMessage(errorReceived, CanaryAPI.errorWarningLevel, true);
+
+				return errorReceived;
+
+			}
+			else
+			{
+
+				CanaryAPI.handleError();
+
+				return;
+
+			}
+
+		}
+
+	}
+
+}
